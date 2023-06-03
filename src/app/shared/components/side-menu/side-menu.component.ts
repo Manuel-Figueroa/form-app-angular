dd9fca8e2461afb80e3d685027667633219a7892
Manuel-Figueroa <figueroagarciamanuel@gmail.com>
2023-06-02 22:44:28 -0600

    bases de formularios dinamicos

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MenuItem } from '../../interfaces/menu-item.iterface';
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: [
+  ]
+})
+export class SideMenuComponent {
+  public reactiveMenu:MenuItem[]=[
+    {title:'Basicos',route:'./reactive/basic'},
+    {title:'Dinamicos',route:'./reactive/dynamic'},
+    {title:'Switches',route:'./reactive/switches'},
+  ]
+
+  public authMenu:MenuItem[]=[
+    {title:'Registro',route:'./auth'},
+  ]
+}
